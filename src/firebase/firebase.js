99b8beb68b8642b3448dff4f5d261f4dd03f80b9
Iandra Bedin <iandrabedin@gmail.com>
2019-11-20 21:04:56 +0100

    Update firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwX9u7o4nnW28o5WAyh_MCAc43fZLZyLs",
-  authDomain: "login-auth-e1e16.firebaseapp.com",
-  databaseURL: "https://login-auth-e1e16.firebaseio.com",
-  projectId: "login-auth-e1e16",
-  storageBucket: "login-auth-e1e16.appspot.com",
-  messagingSenderId: "60452597884",
-  appId: "1:60452597884:web:2f6a055b68e39c227b19f9",
-  measurementId: "G-12Y5G23F88"
+  // YOUR CONFIG VALUES
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
